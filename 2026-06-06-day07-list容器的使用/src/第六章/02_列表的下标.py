d49164ff3c5c912b2f584f731ce6f51398069a88
fgh0901 <1632584079@qq.com>
2026-06-08 12:52:25 +0800

    列表的学习

---
+++ 2026-06-06-day07-list容器的使用/src/第六章/02_列表的下标.py
@@ -0,0 +1,20 @@
+#定义一个列表
+nums = [10,20,30,40]
+
+#测试正索引
+print(nums[0])
+print(nums[1])
+print(nums[2])
+print(nums[3])
+
+
+#测试负索引
+print(nums[-1])
+print(nums[-2])
+print(nums[-3])
+print(nums[-4])
+
+
+#定义一个嵌套列表
+nums2 = [10,20,["nihao","world"],40,50]
+print(nums2[2][1])
